118c79f4fe50cc769a4610ee35fbb7e995c52c9f
Jounaxis <jgs123098@gmail.com>
2025-10-02 17:56:19 -0300

    Adiciona rotas principais e arquivos de estilo ao projeto, substituindo o CSS global e implementando componentes  placeholder de página: Home, Login, Cadastro e Error 404.

--- access-control-cp/src/main.tsx
+++ access-control-cp/src/main.tsx
@@ -1,10 +1,34 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './globals.css'
 import App from './App.tsx'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './routes/Home';
+import Login from './routes/Login';
+import Cad from './routes/Cad';
+import Error from './routes/Error';
+
+const router = createBrowserRouter([
+    {path: "/", element: <App/>, errorElement: <Error/>,
+    children: [
+      { path: "/", element: <Home /> },
+    ]
+  },
+  {
+    path: "/login",
+    element: <Login />,
+    errorElement: <Error />
+  },
+  {
+    path: "/cadastro",
+    element: <Cad />,
+    errorElement: <Error />
+  }
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
---
+++ access-control-cp/src/routes/Cad/index.tsx
@@ -0,0 +1,8 @@
+export default function Cad() {
+
+    return (
+
+        <h1>Cadastro</h1>
+
+    )
+}   
